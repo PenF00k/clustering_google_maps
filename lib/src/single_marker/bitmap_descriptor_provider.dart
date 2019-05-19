3e707ee730fae73e2e65ad0f2499d164e8bc1a44
Виктор Лоскутов <>
2019-05-19 12:19:35 +0300

    fix back :)

--- lib/src/single_marker/bitmap_descriptor_provider.dart
+++ lib/src/single_marker/bitmap_descriptor_provider.dart
@@ -8,7 +8,7 @@ abstract class SingleBitmapDescriptorProvider<T, E> {
 
   PointDescriptor get pointDescriptor => _pointDescriptor;
 
-  Future<Marker> get<E>(E data);
+  Future<Marker> get(E data);
 }
 
 abstract class PointDescriptor<E> {
